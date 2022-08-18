ca2861740f293df30bf95f42fafb449fd1676b5b
sjoedwards <samuel.j.o.edwards@gmail.com>
2022-08-18 18:55:51 +0100

    hints

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -3,7 +3,6 @@ import "./Meme.css";
 
 
 export default function Meme(props) {
-    console.log(props.isLoading)
     return (
         <main>
             <form action="#" method="GET">
@@ -12,8 +11,9 @@ export default function Meme(props) {
                     <input ref={props.bottomText} type="text" id="bottom-text" placeholder="Bottom Text"></input>
                 </div>
                 {/* 8. How do we consume the new property we've passed through to the button? */}
-                <button onClick={e => { props.handleClick(e) }} type="submit" className="meme-submit" disabled={props.isLoading}>Get a new meme image &#128444;</button>
+                <button onClick={e => { props.handleClick(e) }} type="submit" className="meme-submit">Get a new meme image &#128444;</button>
             </form>
+            {/* 13. What could we render here if our new property is true instead of the image? We want to show the user something is loading... */}
             {!props.isLoading ? <img src={props.generatedMeme.url} className="meme--img" /> : <p className="meme--img loading__text">Loading...</p>}
         </main >
     )
